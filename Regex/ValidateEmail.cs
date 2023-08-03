7eac18a01d963b48a44d00adc6a787bff8cf605d
Rithika L <rithika.l@tvsnext.com>
2023-08-03 06:28:42 +0530

    [Rithika] Add : Implemented

--- Regex/ValidateEmail.cs
+++ Regex/ValidateEmail.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-namespace Regexs
+
+namespace Regex
 {
-    public class ValidatePincode
+    public class ValidateEmail
     {
-        string uc1_Regex = "^[0-9]{6}$";
-        string uc4_Regex = "^[0-9]{3}[ ]{0,1}[0-9]{3}$";
+        string UC1_Regex = "^[a-z]{0,}$";
+        string UC2_Regex = "^[@]{1}[a-z]{0,}$";
+        string UC3_Regex = "^[.]{1}[a-z]{2}$";
+        string UC4_Regex_Optional = "^[a-z]+[._+-]{0,1}[a-z]{0,}$";
+        string actualRegex = "^[a-z]+[._+-]{0,1}[a-z]+[@]{1}[a-z]+[.]{1}[a-z]{2,3}([.]{1}[a-z]{2}){0,1}$";
+
         public void UC1_Validate(string input)
         {
-            bool result = Regex.IsMatch(input, uc1_Regex);
+            bool result = Regex.IsMatch(input, UC1_Regex);
             if (result)
                 Console.WriteLine("Validation Successful");
             else
@@ -21,7 +24,7 @@ namespace Regexs
         }
         public void UC2_Validate(string input)
         {
-            bool result = Regex.IsMatch(input, uc1_Regex);
+            bool result = Regex.IsMatch(input, UC2_Regex);
             if (result)
                 Console.WriteLine("Validation Successful");
             else
@@ -29,7 +32,7 @@ namespace Regexs
         }
         public void UC3_Validate(string input)
         {
-            bool result = Regex.IsMatch(input, uc1_Regex);
+            bool result = Regex.IsMatch(input, UC3_Regex);
             if (result)
                 Console.WriteLine("Validation Successful");
             else
@@ -37,13 +40,19 @@ namespace Regexs
         }
         public void UC4_Validate(string input)
         {
-            bool result = Regex.IsMatch(input, uc4_Regex);
+            bool result = Regex.IsMatch(input, UC4_Regex_Optional);
             if (result)
                 Console.WriteLine("Validation Successful");
             else
                 Console.WriteLine("Validation Unsuccessful");
         }
-       
-
+        public void UC5_Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, actualRegex);
+            if (result)
+                Console.WriteLine("Validation Successful");
+            else
+                Console.WriteLine("Validation Unsuccessful");
+        }
     }
 }
